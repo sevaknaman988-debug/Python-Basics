ad44ffc1b08bc45457508e17a58a78791d9634db
Naman <sevaknaman988@gmail.com>
2026-08-03 23:27:55 +0530

    Add student details input program

---
+++ 03_User_Input/05_student_details.py
@@ -0,0 +1,7 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+
+print("My name is", name)
+print("My age is", age)
+print("i am live in", city)
