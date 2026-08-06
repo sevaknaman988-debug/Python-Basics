5790b042fb1de773e695147fbd7ee16c0c14046a
Naman <sevaknaman988@gmail.com>
2026-08-06 22:17:26 +0530

    Add and or operator program

---
+++ 06_Logical_Operators/04_and_or.py
@@ -0,0 +1,5 @@
+a = input("Enter first boolean value (True/False): ") == "True"
+b = input("Enter second boolean value (True/False): ") == "True"
+
+print("AND Result:", a and b)
+print("OR Result:", a or b)
