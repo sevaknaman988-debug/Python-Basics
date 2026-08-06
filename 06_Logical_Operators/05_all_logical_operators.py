dfb70fd00e40d66b9b45407f98e122fab671208f
Naman <sevaknaman988@gmail.com>
2026-08-06 22:19:21 +0530

    Add all logical operators program

---
+++ 06_Logical_Operators/05_all_logical_operators.py
@@ -0,0 +1,6 @@
+a = input("Enter first boolean value (True/False): ") == "True"
+b = input("Enter second boolean value (True/False): ") == "True"
+
+print("AND Result:", a and b)
+print("OR Result:", a or b)
+print("NOT Result of First Value:", not a)
