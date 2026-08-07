027c9f363d1c6c761f9c5d4c20189455f04ed786
Naman <sevaknaman988@gmail.com>
2026-08-07 22:23:51 +0530

    Add largest of two numbers program

---
+++ 07_If_Else/05_largest_of_two_numbers.py
@@ -0,0 +1,9 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 > num2:
+    print("first number is greater")
+elif num2 > num1:
+    print("second number is greater")
+else:
+    print("both numbers are equal")
