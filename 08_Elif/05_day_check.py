907c634ee386ebf44c83b6a133032b7cbeda4399
Naman <sevaknaman988@gmail.com>
2026-08-08 22:50:36 +0530

    Add day check program

---
+++ 08_Elif/05_day_check.py
@@ -0,0 +1,18 @@
+day = int(input("Enter day number (1-7):"))
+
+if day == 1:
+    print("Monday")
+elif day == 2:
+    print("tuesday")
+elif day == 3:
+    print("Wednesday")
+elif day == 4:
+    print("Thursday")
+elif day == 5:
+    print("friday")
+elif day == 6:
+    print("saturday")
+elif day == 7:
+    print("sunday")
+else:
+    print("invalid day number")
